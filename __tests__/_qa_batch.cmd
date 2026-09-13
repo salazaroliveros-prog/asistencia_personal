fc331bf5accd4ecc5e149e561edd711bdb343c5d
salazaroliveros-prog <233714388+salazaroliveros-prog@users.noreply.github.com>
2026-09-13 06:24:45 -0600

    Limpieza profunda del repositorio - Eliminacion de archivos obsoletos

--- __tests__/_qa_batch.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0.."
-call node __tests__\_qa_phase.mjs p6a > __tests__\p6a.log 2>&1
-call node __tests__\_qa_phase.mjs p6b > __tests__\p6b.log 2>&1
-call node __tests__\_qa_phase.mjs p6c > __tests__\p6c.log 2>&1
-echo BATCH_DONE
